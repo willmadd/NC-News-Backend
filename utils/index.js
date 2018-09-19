5577e00513a7cf75619e33e8d8b34e82aa7e2bc1
William Maddicott <williammaddicott@Williams-MacBook-Pro.local>
2018-09-19 11:17:40 +0100

    cors headers tidied up

--- utils/index.js
+++ utils/index.js
@@ -36,7 +36,7 @@ exports.formatComment = (commentData, articleDocs, usersDocs)=>{
     })
 }
 
-exports.allowDatBase = (req, res, next) => {
+exports.allowDataBase = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
