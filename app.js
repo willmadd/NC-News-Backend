4a01d4f62de776e1514a4b42ada9f68dbed5a863
William Maddicott <williammaddicott@Williams-MacBook-Pro.local>
2018-09-19 11:09:32 +0100

    cors headers added

--- app.js
+++ app.js
@@ -14,6 +14,19 @@ app.use(bodyParser.json());
 
 app.use('/api', apiRouter)
 app.use('/', express.static('public/'));
+app.use(function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
 
 app.use((err, req, res, next) => {
   if (err.status === 404) {
